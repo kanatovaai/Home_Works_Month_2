9a3483c0a1b49250f20c43229e9cad51961ba362
Aidai <you@example.com>
2021-05-10 05:24:17 +0600

    first commit

--- homework_1/main.py
+++ homework_1/main.py
@@ -1,1 +1,48 @@
-print('hello world, l, k')
+
+class Fraction:
+
+    def __init__(self, num,  denom):
+        self.num = num
+        self.denom = denom
+
+
+
+
+    def add(self, other):
+        num = self.num * other.denom + self.denom * other.num
+        denom = self.denom * other.denom
+        print(num)
+        print('-')
+        print(denom)
+
+
+    def divide(self, other):
+        num = self.num * other.denom
+        denom = self.denom * other.num
+        print('---------------------')
+        print(num)
+        print('-')
+        print(denom)
+
+    def multiply(self, other):
+        num = self.num * other.num
+        denom = self.denom * other.denom
+        print('---------------------')
+        print(num)
+        print('-')
+        print(denom)
+
+    def subtract(self, other):
+        num = self.num * other.denom - self.denom * other.num
+        denom = self.denom * other.denom
+        print('---------------------')
+        print(num)
+        print('-')
+        print(denom)
+
+f1 = Fraction(10, 5)
+f2 = Fraction(20, 5)
+f1.add(f2)
+f1.divide(f2)
+f1.multiply(f2)
+f1.subtract(f2)
